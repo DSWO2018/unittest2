1983ba0976315a731df6e8c1fca4db4083af04f7
Elpala <luis.pala1997@gmail>
2018-10-30 17:28:03 -0600

    Completed interfaces and implementation of Cart

--- src/main/java/interfaces/impl/MyCart.java
+++ src/main/java/interfaces/impl/MyCart.java
@@ -6,49 +6,106 @@ import java.util.ArrayList;
 
 public class MyCart implements interfaces.Cart {
     ArrayList<ProductStructure> productList;
-    public MyCart(){
+
+    public MyCart() {
         productList = new ArrayList<ProductStructure>();
     }
+
     public String addProduct(Product product) {
-        boolean productExists = false;
-        if (product == null){
+        if (product == null) {
             return "Producto No Se Pudo Agregar";
         }
-        for (ProductStructure producttemp: productList
-             ) {
-            if (producttemp.prod.getID() == product.getID()){
+        for (ProductStructure producttemp : productList) {
+            if (producttemp.product.getID() == product.getID()) {
                 producttemp.Qty += 1;
-                productExists = true;
-                break;
+                return "Producto Ya Existe En El Carrito";
             }
         }
-        if( productExists == false ){
-            productList.add(new ProductStructure(product));
-            return "Producto Agregado Exitosamente";
-        }
-        else{
-            return "Producto Ya Existe En El Carrito";
-        }
+        productList.add(new ProductStructure(product));
+        return "Producto Agregado Exitosamente";
     }
 
     public String removeProduct(Product product) {
-        for (ProductStructure producttemp : productList
-                ) {
-            {
-                if (producttemp.prod.getID() == product.getID()) {
-                    productList.remove(product);
-                    return "Producto Removido Exitosamente";
-                }
+        for (ProductStructure productTemp : productList) {
+            if (productTemp.product.getID() == product.getID()) {
+                productList.remove(productTemp);
+                return "Producto Removido Exitosamente";
             }
         }
         return "Producto No Se Encuentra En El Carrito";
     }
-}
-class ProductStructure{
-    Product prod;
-    int Qty = 0;
-    ProductStructure(Product receivedProduct){
-        prod = receivedProduct;
-        Qty += 1;
+
+    public double calculateTotal() {
+        double total =  calculateSubtotal() + calculateTax();
+        return total;
+    }
+
+    public double calculateSubtotal() {
+        double subtotal = 0;
+        for (ProductStructure products : productList) {
+            subtotal += products.product.getPrice() * products.Qty;
+        }
+        return subtotal;
+    }
+
+    public double calculateTax() {
+        double tax = 0;
+        double iva = .16;
+        for (ProductStructure products : productList) {
+            if (products.product.isTaxeable()){
+                tax += products.product.getPrice() * products.Qty * iva;
+            }
+        }
+        return tax;
+    }
+
+    public double calculateLineItemSubtotal(int pos) {
+        double itemSubtotal;
+        try{
+            ProductStructure products = productList.get(pos);
+            itemSubtotal = products.product.getPrice() * products.Qty;
+        }catch (Exception e){
+            itemSubtotal = 0;
+        }
+        return itemSubtotal;
+    }
+
+    public int addQuantityToLineItem(int pos, int qty) {
+        int newQty;
+        try{
+            ProductStructure products = productList.get(pos);
+            products.Qty+=qty;
+            newQty = products.Qty;
+        }catch (Exception e){
+            newQty = 0;
+        }
+        return newQty;
+    }
+
+    public int removeQuantityToLineItem(int pos, int qty) {
+        int newQty;
+        try{
+            ProductStructure products = productList.get(pos);
+            products.Qty-=qty;
+            if(products.Qty <=0 ){
+                productList.remove(products);
+                newQty = 0;
+            }else {
+                newQty = products.Qty;
+            }
+        }catch (Exception e){
+            newQty = 0;
+        }
+        return newQty;
+    }
+
+    class ProductStructure {
+        Product product;
+        int Qty = 0;
+
+        ProductStructure(Product receivedProduct) {
+            product = receivedProduct;
+            Qty += 1;
+        }
     }
 }
