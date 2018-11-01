20849bf46ae4c2ed2a5d02ff44b0bc38b5efa36a
PedroGtz16 <37647681+PedroGtz16@users.noreply.github.com>
2018-11-01 16:56:21 -0600

    Add files via upload

--- src/main/java/interfaces/impl/MyCart.java
+++ src/main/java/interfaces/impl/MyCart.java
@@ -1,15 +1,14 @@
 package interfaces.impl;
 
-import interfaces.Product;
 
 import java.util.ArrayList;
 
-public class MyCart implements interfaces.Cart {
+public class MyCart implements iCart {
     ArrayList<ProductStructure> productList;
     public MyCart(){
         productList = new ArrayList<ProductStructure>();
     }
-    public String addProduct(Product product) {
+    public String addProduct(iProduct product) {
         boolean productExists = false;
         if (product == null){
             return "Producto No Se Pudo Agregar";
@@ -31,23 +30,57 @@ public class MyCart implements interfaces.Cart {
         }
     }
 
-    public String removeProduct(Product product) {
+    public String removeProduct(iProduct product) {
         for (ProductStructure producttemp : productList
                 ) {
             {
                 if (producttemp.prod.getID() == product.getID()) {
-                    productList.remove(product);
-                    return "Producto Removido Exitosamente";
+                    if(producttemp.Qty<2) {
+                        productList.remove(product);
+                        return "Producto Removido Exitosamente";
+                    }
+                    else {
+                        producttemp.Qty--;
+                        return "Producto Removido Exitosamente pero aun queda alguno en el carro!";
+                    }
                 }
             }
         }
         return "Producto No Se Encuentra En El Carrito";
     }
+
+    public double calculateTotal() {
+        return 0;
+    }
+
+    public double calculateSubTotal() {
+        return 0;
+    }
+
+    public double calculateTax(double val) {
+        val=val+val*.16;
+        return val;
+    }
+
+    public double calculateLineItemSubTotal(int pos) {
+        return 0;
+    }
+
+    public int addQuantityToLineItem(int pos, int qty) {
+        return 0;
+    }
+
+    public int removeQuantityToLineItem(int pos, int qty) {
+        return 0;
+    }
+
 }
 class ProductStructure{
-    Product prod;
+    iProduct prod;
     int Qty = 0;
-    ProductStructure(Product receivedProduct){
+    int price=0;
+    boolean tax;
+    ProductStructure(iProduct receivedProduct){
         prod = receivedProduct;
         Qty += 1;
     }
