672b1179fa19f1ee3776ce8d592a602aec78f453
ManuelSanchez25 <39495445+ManuelSanchez25@users.noreply.github.com>
2018-10-31 20:58:28 -0600

    First Commit

--- src/main/java/interfaces/Cart.java
+++ src/main/java/interfaces/Cart.java
@@ -1,7 +1,57 @@
 package interfaces;
 
-public interface Cart {
-    String addProduct(Product product);
 
+/**
+ * interface.
+ */
+public interface Cart {
+    /**
+     *
+     * @param product producto
+     * @return resultado de la operacion.
+     */
+    String addProduct(Product product);
+    /**
+     *
+     * @param product p.
+     * @return res.
+     */
     String removeProduct(Product product);
+    /**
+     *
+     * @return res.
+     */
+    double calculateTotal();
+    /**
+     *
+     * @return res.
+     */
+    double calculateSubtotal();
+    /**
+     *
+     * @return res.
+     */
+    double calculateTax();
+    /**
+     *
+     * @param pos p.
+     * @return res.
+     */
+    double calculateLineItemSubtotal(int pos);
+
+    /**
+     *
+     * @param pos
+     * @param qty
+     * @return res.
+     */
+    int addQuantityToLineItem(int pos, int qty);
+
+    /**
+     *
+     * @param pos
+     * @param qty
+     * @return res.
+     */
+    int removeQuantityToLineItem(int pos, int qty);
 }
--- src/main/java/interfaces/Product.java
+++ src/main/java/interfaces/Product.java
@@ -1,5 +1,60 @@
 package interfaces;
 
+/**
+ * interface.
+ */
 public interface Product {
-    int getID();
+    /**
+     *
+     * @return r
+     */
+    int getId();
+
+    /**
+     *
+     * @return r
+     */
+    double getPrice();
+
+    /**
+     *
+     * @return
+     */
+    String getDescription();
+
+    /**
+     *
+     * @param price
+     */
+    void setPrice(double price);
+
+    /**
+     *
+     * @return
+     */
+    boolean isTaxeable();
+
+    /**
+     *
+     * @param taxeable
+     */
+    void setTaxeable(boolean taxeable);
+
+    /**
+     *
+     * @return
+     */
+    String getName();
+
+    /**
+     *
+     * @param name
+     */
+    void setName(String name);
+
+    /**
+     *
+     * @return
+     */
+    String getID();
 }
