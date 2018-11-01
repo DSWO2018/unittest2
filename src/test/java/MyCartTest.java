b01686e343e3b9d9d481ad69ace057277418a8fb
ManuelSanchez25 <39495445+ManuelSanchez25@users.noreply.github.com>
2018-10-31 20:59:42 -0600

    First Commit

--- src/test/java/MyCartTest.java
+++ src/test/java/MyCartTest.java
@@ -1,18 +1,15 @@
 import interfaces.Cart;
 import interfaces.Product;
-import interfaces.impl.MyCart;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.mock;
+import static org.mockito.when;
 
 public class MyCartTest {
     Cart myCart;
     Product mockedProduct;
     Product secondMockedProduct;
-
 
     @Before
     public void setUp(){
@@ -21,7 +18,14 @@ public class MyCartTest {
         secondMockedProduct = mock(Product.class);
         when(mockedProduct.getID()).thenReturn(1);
         when(secondMockedProduct.getID()).thenReturn(2);
+
+        when(mockedProduct.getPrice()).thenReturn(98.99);
+        when(mockedProduct.isTaxeable()).thenReturn(true);
+        when(secondMockedProduct.getPrice()).thenReturn(1.01);
+        when(secondMockedProduct.isTaxeable()).thenReturn(false);
+
     }
+
 
     @Test
     public void testAgregadoExitosamente(){
@@ -57,6 +61,97 @@ public class MyCartTest {
         Assert.assertEquals("Producto Removido Exitosamente",result2);
 
     }
+    @Test
+    public void testCalculateSubtotal(){
+        myCart.addProduct(mockedProduct);
+        myCart.addProduct(secondMockedProduct);
+        double result  = myCart.calculateSubtotal();
+        double subtotal = 98.99 +1.01;
+        Assert.assertEquals(subtotal,result,result);
+    }
 
+    @Test
+    public void testCalculateTax(){
+        myCart.addProduct(mockedProduct);
+        myCart.addProduct(secondMockedProduct);
+        double result  = myCart.calculateTax();
+        double subtotal = 98.99 * .16;
+        Assert.assertEquals(subtotal,result,result);
+    }
 
+    @Test
+    public void testCalculateTotal(){
+        myCart.addProduct(mockedProduct);
+        myCart.addProduct(secondMockedProduct);
+        double result  = myCart.calculateTotal();
+        double subtotal = (98.99 * .16) + 1.01;
+        Assert.assertEquals(subtotal,result,result);
+    }
+    @Test
+    public void testCalculateLineSubtotal(){
+        myCart.addProduct(mockedProduct);
+        myCart.addProduct(secondMockedProduct);
+        double result  = myCart.calculateLineItemSubtotal(0);
+        double subtotal = 99.99 ;
+        Assert.assertEquals(subtotal,result,result);
+    }
+    @Test
+    public void testCalculateLineSubtotalNull(){
+        myCart.addProduct(mockedProduct);
+        myCart.addProduct(secondMockedProduct);
+        double result  = myCart.calculateLineItemSubtotal(2);
+        double subtotal = 0 ;
+        Assert.assertEquals(subtotal,result,result);
+    }
+
+    @Test
+    public void testAddQuantityToLineItem(){
+        myCart.addProduct(mockedProduct);
+        myCart.addProduct(mockedProduct);
+        myCart.addProduct(secondMockedProduct);
+        int result  = myCart.addQuantityToLineItem(0,2);
+        int qty = 2 + 2 ;
+        Assert.assertEquals(qty,result);
+    }
+
+    @Test
+    public void testAddQuantityToLineItemNull(){
+        myCart.addProduct(mockedProduct);
+        myCart.addProduct(mockedProduct);
+        myCart.addProduct(secondMockedProduct);
+        int result  = myCart.addQuantityToLineItem(3,2);
+        int qty = 0;
+        Assert.assertEquals(qty,result);
+    }
+
+    @Test
+    public void testRemoveQuantityToLineItem(){
+        myCart.addProduct(mockedProduct);
+        myCart.addProduct(mockedProduct);
+        myCart.addProduct(secondMockedProduct);
+        int result  = myCart.removeQuantityToLineItem(0,1);
+        int qty = 1;
+        Assert.assertEquals(qty,result);
+    }
+    @Test
+    public void testRemoveQuantityToLineItemNull(){
+        myCart.addProduct(mockedProduct);
+        myCart.addProduct(mockedProduct);
+        myCart.addProduct(secondMockedProduct);
+        int result  = myCart.removeQuantityToLineItem(3,1);
+        int qty = 0;
+        Assert.assertEquals(qty,result);
+    }
+
+    @Test
+    public void testRemoveQuantityToLineItemOvertake(){
+        myCart.addProduct(mockedProduct);
+        myCart.addProduct(mockedProduct);
+        myCart.addProduct(secondMockedProduct);
+        int result  = myCart.removeQuantityToLineItem(0,5);
+        int qty = 0;
+        Assert.assertEquals(qty,result);
+    }
 }
+
+
